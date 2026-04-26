b6cbb33256aa1fe7d0cbe8c9013af6e57d9695aa
Clarence Mah <clarence.k.mah@gmail.com>
2026-04-26 01:19:35 -0700

    experiment: tune joblib loft={'batch_size': 1} tile={'batch_size': 2}

--- polyplot/_tune.py
+++ polyplot/_tune.py
@@ -6,8 +6,8 @@ Autoresearch scripts may rewrite ``EXTRA_*`` dicts and commit; public API is unc
 
 from __future__ import annotations
 
-EXTRA_LOFT_PARALLEL: dict = {}
-EXTRA_TILE_PARALLEL: dict = {'batch_size': 16}
+EXTRA_LOFT_PARALLEL: dict = {'batch_size': 1}
+EXTRA_TILE_PARALLEL: dict = {'batch_size': 2}
 
 
 def loft_parallel_kw() -> dict:
